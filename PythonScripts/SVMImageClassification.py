0dbdc3cd979fe530babe2bc29fdc88a150ffa352
ahmadbarzak <76200661+ahmadbarzak@users.noreply.github.com>
2023-06-18 19:11:08 +1200

    Added Explainability Tasks due 19th

---
+++ PythonScripts/SVMImageClassification.py
@@ -0,0 +1,19 @@
+# Task for Caleb:
+# 1: Load training data for image classification.
+
+# 2: Apply SVM model using Scikit learn, choose hyperparameters
+# at your own discretion, or via hyperparameter tuning e.g use gridsearch.
+
+# 3: Apply Lime explainability object and try to plot the results
+# for one particular prediction.
+
+# 4: Investigate how you did this and whether you had to modify the
+# structure of the model code to do task 3.
+
+# 5: try multiple different predictions with lime and write what you find.
+
+# 6: Extra for experts: Try using a different model such as bayes or xgboost/random forest
+# and try see how your explainability evaluations change for the same image dataset and predictions.
+
+# 7: Document the following: What parts of your code had to change to achieve this?
+# Do you think this code can be easily swapped out for other models for example?
