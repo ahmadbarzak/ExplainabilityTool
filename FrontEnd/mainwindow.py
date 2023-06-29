c77abc9a5cc3c43271ff81c9c2eafec10d10ce4f
ahmadbarzak <76200661+ahmadbarzak@users.noreply.github.com>
2023-06-30 03:06:51 +1200

    Added Gallery Feature

--- FrontEnd/mainwindow.py
+++ FrontEnd/mainwindow.py
@@ -1,6 +1,6 @@
 # This Python file uses the following encoding: utf-8
 from PyQt5.QtWidgets import *
-from PyQt5.QtGui import QFont
+from PyQt5.QtGui import QFont, QPixmap, QPainter
 from PyQt5.uic import loadUi
 import sys
 import numpy as np
@@ -14,6 +14,8 @@ import random
 from sklearn.svm import SVC
 from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
+from PIL import Image
+import matplotlib.image
 
 def transition(context):
     widget.addWidget(context)
@@ -21,25 +23,15 @@ def transition(context):
     widget.removeWidget(widget.currentWidget())
     widget.setCurrentIndex(nextIndex)
 
+
 class MainMenu(QMainWindow):
     def __init__(self, parent=None):
         #Sets labels etc
         super(MainMenu, self).__init__()
         loadUi('FrontEnd/UI/MainMenu.ui', self)
-        self.title = QLabel('P4P XAI Tool', self)
-        
-        self.title.setGeometry(50,50,1000,75)
-        self.title.setFont(QFont("Comic Sans MS", 40))
-        self.title.move(100, 30)
 
-        #This initialises the ui
-        self.initUI()
-
-    def initUI(self):
-        #labels
-        self.lbl = QLabel('', self)
-        self.lbl.move(75, 200)
         self.defaultCLF.clicked.connect(lambda: transition(DataViewer()))
+        self.loadCLF.clicked.connect(lambda: transition(Gallery()))
         self.trainCLF.clicked.connect(lambda: transition(DataSelect()))
         self.exit.clicked.connect(self.closeApp)
         self.show()
@@ -62,14 +54,14 @@ class DataSelect(QWidget):
         self.show()
 
 class DataViewer(QWidget):
-    def __init__(self):
+    def __init__(self, x_test=None, y_test=None, clf=None):
         super(DataViewer, self).__init__()
         loadUi('FrontEnd/UI/DataViewer.ui', self)
         self.pipe = None
         self.x_train, self.x_test, self.y_train, self.y_test = None, None, None, None
         self.backButton.clicked.connect(lambda: transition(MainMenu()))
         self.classifyButton.clicked.connect(self.defaultClassify)
-        self.explainButton.clicked.connect(self.explain)
+        self.explainButton.clicked.connect(lambda: transition(Gallery(self.x_test, self.y_test, self.pipe)))
         self.plotButton.clicked.connect(self.plot)
         self.show()
 
@@ -104,6 +96,11 @@ class DataViewer(QWidget):
         print("please wait a little while the classifier is fit to the data")
         self.pipe.fit(self.x_train, self.y_train)
         print(self.pipe.score(self.x_test, self.y_test))
+
+        for i in range(120):
+            im = Image.fromarray(self.x_test[i])
+            im.save("Datasets/sampledata/"+str(i)+".png")
+
         print("done")
 
     def plot(self):
@@ -131,6 +128,82 @@ class DataViewer(QWidget):
         ax2.imshow(label2rgb(3-mask,temp, bg_label = 0), interpolation = 'nearest')
         ax2.set_title('Positive/Negative Regions for {}'.format(self.y_test[0]))
         plt.show()
+
+
+class PicButton(QAbstractButton):
+    def __init__(self, pixmap, id=0, parent=None):
+        super(PicButton, self).__init__(parent)
+        self.pixmap = pixmap
+        self.id = id
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.drawPixmap(event.rect(), self.pixmap)
+
+    def sizeHint(self):
+        return self.pixmap.size()
+    
+    def getID(self):
+        return self.id
+    
+
+class Gallery(QWidget):
+
+    def __init__(self, x_test, y_test, clf):
+        #Sets labels etc
+        super(Gallery, self).__init__()
+        # loadUi('FrontEnd/UI/Gallery.ui', self)
+
+        back = QPushButton("Back", self)
+        back.clicked.connect(lambda: transition(DataViewer(x_test, y_test, clf)))
+
+        gridLayout = QGridLayout()
+        groupBox = QGroupBox()
+
+        for i in range(120):
+            button = PicButton(QPixmap("Datasets/sampledata/" + str(i) + ".png"))
+            button.id = i
+            button.clicked.connect(lambda: self.explain(x_test, y_test, clf))
+            # button = QPushButton(str(i), self)
+            gridLayout.addWidget(button, (i)//3, (i)%3)
+        
+        groupBox.setLayout(gridLayout)
+
+        scroll = QScrollArea()
+        scroll.setWidget(groupBox)
+        scroll.setWidgetResizable(True)
+        scroll.setFixedHeight(400)
+
+        layout = QVBoxLayout()
+        layout.addWidget(scroll)
+
+        self.setLayout(layout)
+
+
+        # self.defaultCLF.clicked.connect(lambda: transition(DataViewer()))
+        # self.trainCLF.clicked.connect(lambda: transition(DataSelect()))
+        # self.exit.clicked.connect(self.closeApp)
+        self.show()
+
+
+    def explain(self, x_test, y_test, clf):
+        id = self.sender().getID()
+        explainer = lime_image.LimeImageExplainer(verbose = False)
+        segmenter = SegmentationAlgorithm('quickshift', kernel_size=1, max_dist=200, ratio=0.2)
+
+        explanation = explainer.explain_instance(x_test[id], 
+                                        classifier_fn = clf.predict_proba, 
+                                        top_labels=10, hide_color=0, num_samples=2000, segmentation_fn=segmenter)
+
+        temp, mask = explanation.get_image_and_mask(y_test[id], positive_only=True, num_features=10, hide_rest=False, min_weight = 0.01)
+        fig, (ax1, ax2) = plt.subplots(1,2, figsize = (8, 4))
+        ax1.imshow(label2rgb(mask,temp, bg_label = 0), interpolation = 'nearest')
+        ax1.set_title('Positive Regions for {}'.format(y_test[id]))
+        temp, mask = explanation.get_image_and_mask(y_test[id], positive_only=False, num_features=10, hide_rest=False, min_weight = 0.01)
+        ax2.imshow(label2rgb(3-mask,temp, bg_label = 0), interpolation = 'nearest')
+        ax2.set_title('Positive/Negative Regions for {}'.format(y_test[id]))
+        plt.show()
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
