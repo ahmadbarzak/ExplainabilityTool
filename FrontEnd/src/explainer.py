3526ad6a6d44b3fa6ae421da87dc00a557d97b7a
ahmadbarzak <76200661+ahmadbarzak@users.noreply.github.com>
2023-09-27 19:44:05 +1300

    added super scuffed keras implementation

--- FrontEnd/src/explainer.py
+++ FrontEnd/src/explainer.py
@@ -22,6 +22,8 @@ class Explainer(QWidget):
         self.go.hide()
         # self.hpSlider.move(30, 60)
 
+        self.explain_keras()
+
         self.modelData = modelData
         self.varLabel = QLabel("Variable " + modelData["var"] + " values:", self)
         font = QFont()
@@ -121,3 +123,66 @@ class Explainer(QWidget):
         ax2.set_title("Explanation after varying " + str(self.modelData["var"]) + " to " + str(self.modelData["vals"][self.currentClf]))
         plt.show()
     
+
+
+    def explain_keras(self):
+
+        import tensorflow as tf
+        import numpy as np
+        from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+        from tensorflow.keras.models import Sequential
+        from tensorflow.keras.preprocessing.image import ImageDataGenerator
+        import lime
+        from lime import lime_image
+
+
+        # Define data directories
+        train_dir = '/home/caleb/Desktop/p4p/ExplainabilityTool/Datasets/keras_datasets/train'
+        validation_dir = '/home/caleb/Desktop/p4p/ExplainabilityTool/Datasets/keras_datasets/valid'
+
+        # Define data generators
+        train_datagen = ImageDataGenerator(rescale=1.0/255.0)
+        validation_datagen = ImageDataGenerator(rescale=1.0/255.0)
+
+        train_generator = train_datagen.flow_from_directory(
+            train_dir,
+            target_size=(150, 150),
+            batch_size=32,
+            class_mode='categorical'
+        )
+
+        validation_generator = validation_datagen.flow_from_directory(
+            validation_dir,
+            target_size=(150, 150),
+            batch_size=32,
+            class_mode='categorical'
+        )
+
+
+        loaded_model = tf.keras.models.load_model("/home/caleb/Desktop/p4p/ExplainabilityTool/testing/first_keras_test.h5")
+        # Print the model summary
+        loaded_model.summary()
+        
+        # Select an image for explanation
+        image = validation_generator[0][0][0]  # Replace with the image you want to explain
+        # Explain the prediction
+        explainer = lime_image.LimeImageExplainer(verbose = False) 
+
+        explanation = explainer.explain_instance(image, loaded_model.predict)
+        segmenter = SegmentationAlgorithm('quickshift', kernel_size=1, max_dist=200, ratio=0.2)
+        explanation = explainer.explain_instance(image, 
+                                                 classifier_fn = loaded_model.predict, 
+                                                 top_labels=10,
+                                                 hide_color=0,
+                                                 num_samples=2000,
+                                                 segmentation_fn=segmenter)
+
+        # Explain the prediction
+        # explanation = explainer.explain_instance(image, predict_fn, top_labels=5, hide_color=0, num_samples=1000)
+
+        temp, mask = explanation.get_image_and_mask(explanation.top_labels[0], positive_only=False, num_features=10, hide_rest=False, min_weight=0.01)
+        plt.imshow(mark_boundaries(temp / 2 + 0.5, mask))
+        # temp, mask = explanation.get_image_and_mask(y_test[id], positive_only=False, num_features=10, hide_rest=False, min_weight = 0.01)
+        # fig, (ax1, ax2) = plt.subplots(1,2, figsize = (8, 4))
+        # ax1.imshow(mark_boundaries(temp, mask))
+        
